82e93dd011d7d02217b6057117e5ee8ef09ec4d5
mangeshipatil <thisismangeshi@gmail.com>
2022-12-12 16:43:28 +0530

    added main-page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from "./components/nav";
 import Main from "./components/main";
+import Second from "./components/second";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
     <NavBar/>
     <Main/>
+    <Second/>
     </>
   );
 }
---
+++ src/components/second.jsx
@@ -0,0 +1,46 @@
+import "./style.css"
+import { BsTrophy } from "react-icons/bs"
+import {CiTrophy} from "react-icons/ci"
+import { CiDatabase } from "react-icons/ci"
+import { CiWavePulse1 } from "react-icons/ci"
+
+const Second = () => {
+    return (
+        <>
+            <div class="container  second-page">
+                <div class="row">
+                    <div class="col">
+                        <div className="wave-icon icns">
+                            <CiWavePulse1 />
+                        </div>
+                        <div className="wave-text">
+                            Turn systems of records into systems of insight
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div className="trophy-icon icns">
+                            <CiTrophy/>
+                        </div>
+                        <div className="trophy-text">
+                            Drive targeted interventions, improve outcomes
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div className="db-icon icns">
+                            <CiDatabase />
+                        </div>
+                        <div className="db-text">
+                            Leverage data across the customer lifecycle
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+
+
+
+export default Second
